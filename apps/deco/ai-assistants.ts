d91631317c35f5541de0d5b42bd1201429c4e8c4
04 Khumanthem Lanthoiba Meitei <khumanthemlanthoiba@gmail.com>
2024-07-13 12:37:38 +0000

    Let's get started!

---
+++ apps/deco/ai-assistants.ts
@@ -0,0 +1,2 @@
+export { default } from "apps/ai-assistants/mod.ts";
+export * from "apps/ai-assistants/mod.ts";
---
+++ routes/_app.tsx
@@ -0,0 +1,37 @@
+import { asset, Head } from "$fresh/runtime.ts";
+import { defineApp } from "$fresh/server.ts";
+import { Context } from "deco/deco.ts";
+import Theme from "../sections/Theme/Theme.tsx";
+
+export default defineApp(async (_req, ctx) => {
+  const revision = await Context.active().release?.revision();
+
+  return (
+    <>
+      {/* Include default fonts and css vars */}
+      <Theme colorScheme="any" />
+
+      {/* Include Icons and manifest */}
+      <Head>
+        {/* Enable View Transitions API */}
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `@view-transition { navigation: auto; }`,
+          }}
+        />
+
+        {/* Tailwind v3 CSS file */}
+        <link
+          href={asset(`/styles.css?revision=${revision}`)}
+          rel="stylesheet"
+        />
+
+        {/* Web Manifest */}
+        <link rel="manifest" href={asset("/site.webmanifest")} />
+      </Head>
+
+      {/* Rest of Preact tree */}
+      <ctx.Component />
+    </>
+  );
+});
